567ff91c8951f1b17b141fa3990932058b7f8d25
freemanbach <flo@radford.edu>
2018-11-24 16:26:14 -0500

    added something interesting to test

---
+++ examples/Sample.java
@@ -0,0 +1,24 @@
+/**  
+ *
+ * @author Freeman
+ * @version 0.0.1 
+ * @date 2018.11.24
+ * @vim set tabstop=4
+ *
+ */
+
+import java.util.*;
+import java.lang.*;
+
+class IOHobbitException extends java.lang.Exception {
+}
+
+public class Sample {
+	public static void main(String[] args) throws IOHobbitException {
+		
+		String mess = "Hello Planet, Earth!";
+		System.out.println(mess);
+
+		System.exit(0);
+	}
+}
